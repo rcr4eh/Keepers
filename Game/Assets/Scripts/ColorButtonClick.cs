e8b4c3cac7debce27270ce6ac6607b18a0966663
Clayton Smith <cps5aa@virginia.edu>
2016-11-19 14:34:15 -0500

    Change Team 2 Color
    Changes team 2 color to green with ColorButtonClick

---
+++ Game/Assets/Scripts/ColorButtonClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ColorButtonClick : MonoBehaviour
+{
+	public Map map;
+
+	public void Click ()
+	{
+		GameObject[] play2col = GameObject.FindGameObjectsWithTag ("Player 2 Color");
+		foreach (GameObject color in play2col) {
+			MeshRenderer Mesh = color.GetComponent<MeshRenderer> ();
+			GameObject greenTeam = GameObject.Find ("GreenTeam");
+			MeshRenderer Mesh2 = greenTeam.GetComponent<MeshRenderer> ();
+			Mesh.material = Mesh2.material;
+		}
+	}
+}
