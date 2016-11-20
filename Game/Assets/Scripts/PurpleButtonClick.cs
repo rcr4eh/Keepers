15ea0f11ce519f8888d22d60e6411b9fe65b9c20
Clayton Smith <cps5aa@virginia.edu>
2016-11-20 04:03:24 -0500

    Purple Team Buttons
    I added in the purple team’s button for Team 2

---
+++ Game/Assets/Scripts/PurpleButtonClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PurpleButtonClick : MonoBehaviour
+{
+	public Map map;
+
+	public void Click ()
+	{
+		GameObject[] players2 = GameObject.FindGameObjectsWithTag ("Player 2 Color");
+		foreach (GameObject play in players2) {
+			MeshRenderer color = play.GetComponent<MeshRenderer> ();
+			GameObject colorChangeUnit = GameObject.Find ("PurpleTeam");
+			MeshRenderer colorChange = colorChangeUnit.GetComponent<MeshRenderer> ();
+			color.material = colorChange.material;
+		}
+	}
+}
